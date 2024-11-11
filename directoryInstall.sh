86f2bc8b416c9848fcbcbc18f78dfc78f854a62f
Alvaro <alvarodc@uma.es>
2024-11-11 09:15:11 +0100

    Initial commit

---
+++ directoryInstall.sh
@@ -0,0 +1,50 @@
+if [[ $# -eq 1 ]];
+then
+  echo "Analyzing directory $1";
+  cd $1 || { echo "Failed to change directory to $1"; exit 1; }
+  directoryList=()
+  if [[ $? -eq 0 ]];
+  then
+        
+    for file in $(ls)
+    do
+	    echo "The file: $file";
+	    name_without_extension=$(basename "$file" | sed 's/\(.*\)\..*/\1/' | sed 's/\(.*\)\..*/\1/') 
+	    echo "Without $name_without_extension"
+	    extension=$(echo "$file" | sed "s/$name_without_extension//")
+	    echo "The extension is $extension"
+	    if [[ "${extension}" =  ".tar.gz" ]]; then
+        echo "Extracting file $file";
+
+        mkdir $name_without_extension;
+        if [[ $? -eq 0 ]];
+        then
+            tar -xzvf $file -C $name_without_extension;
+            echo "Content extracted to ${name_without_extension}";
+            directoryList+=("$name_without_extension");
+        fi
+
+      else
+        echo "The file should be .tar.gz";
+      fi
+    done
+
+        # Proceed with the installation
+    if [[ ${#directoryList} -ne 0  ]];
+    then
+      echo "Number of files extracted: ${#directoryList}";
+      installFile="setup.py";
+      for package in $directoryList:
+      do
+          cd $1;
+
+          echo "Trying to install ${package}";
+          cd $package;
+
+      done
+    fi
+
+	fi
+else
+    echo "Command usage with one arg";
+fi
