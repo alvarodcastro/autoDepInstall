96dfb0a18617f78093c4ce77208e872f1ec458ab
Alvaro <alvarodc@uma.es>
2024-11-11 10:54:06 +0100

    Debug strings added

--- directoryInstall.sh
+++ directoryInstall.sh
@@ -39,10 +39,12 @@ then
       for package in $directoryList:
       do
         installationDir=$(find ${package} -name "src")
+        base_path=$(find . -type f -name "setup.py" -o -name "pyproject.toml" -o -name "requirements.txt" -o -name "README.*" -o -name "LICENSE*" -o -name "setup.cfg" -o -type d -name "src" -o -type d -name "tests" -o -type d -name ".git" 2>/dev/null | sed -E 's|/[^/]+$||' | sort -u | head -n 1)
+
           # cd $1;
 
-          echo "Trying to install ${package} at the following directory: ${installationDir}";
-          python -m build ${installationDir}
+          echo "Trying to install ${package} at the following directory: ${base_path}";
+          python -m build ${base_path}
 
       done
     fi
